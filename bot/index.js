889750a97cb9dd39c79e4522d47494bf26b09d8d
ivanturianytsia <vanyaturianitsa@gmail.com>
2020-07-15 20:52:54 +0200

    Prepare to Heroku test deployment

--- bot/index.js
+++ bot/index.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const Telegraf = require('telegraf')
-const storage = require('./storage')
+// const storage = require('./storage')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.on('photo', async ctx => {
   try {
-    const fileId = ctx.update.message.photo[1].file_id
-    const url = await ctx.telegram.getFileLink(fileId)
-    await storage.uploadPhoto(url, `${ctx.update.message.date}.jpg`)
+    // const fileId = ctx.update.message.photo[1].file_id
+    // const url = await ctx.telegram.getFileLink(fileId)
+    // await storage.uploadPhoto(url, `${ctx.update.message.date}.jpg`)
     ctx.reply('Saved!')
   } catch (err) {
     console.log(err)
@@ -18,3 +18,6 @@ bot.on('photo', async ctx => {
 })
 
 bot.launch()
+
+bot.telegram.setWebhook(process.env.WEBHOOK_URL)
+bot.startWebhook(process.env.WEBHOOK_PATH, null, process.env.WEBHOOK_PORT)
