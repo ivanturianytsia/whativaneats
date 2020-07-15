a30c103b22c7075bd8bf2c0766716ed1404a4bf2
ivanturianytsia <vanyaturianitsa@gmail.com>
2020-07-15 21:43:34 +0200

    Allow testing on local/setting webhook

--- bot/index.js
+++ bot/index.js
@@ -1,15 +1,15 @@
 'use strict'
 
 const Telegraf = require('telegraf')
-// const storage = require('./storage')
+const storage = require('./storage')
 
 const bot = new Telegraf(process.env.BOT_TOKEN)
 
 bot.on('photo', async ctx => {
   try {
-    // const fileId = ctx.update.message.photo[1].file_id
-    // const url = await ctx.telegram.getFileLink(fileId)
-    // await storage.uploadPhoto(url, `${ctx.update.message.date}.jpg`)
+    const fileId = ctx.update.message.photo[1].file_id
+    const url = await ctx.telegram.getFileLink(fileId)
+    await storage.uploadPhoto(url, `${ctx.update.message.date}.jpg`)
     ctx.reply('Saved!')
   } catch (err) {
     console.log(err)
@@ -17,7 +17,10 @@ bot.on('photo', async ctx => {
   }
 })
 
-bot.launch()
-
-bot.telegram.setWebhook(`${process.env.WEBHOOK_HOST}:${process.env.PORT}/upload`)
-bot.startWebhook('/upload', null, process.env.PORT)
+if (process.env.LOCAL) {
+  bot.launch()
+} else {
+  const webhookUrl = `${process.env.WEBHOOK_HOST}/upload`
+  bot.telegram.setWebhook(webhookUrl)
+  bot.startWebhook('/upload', null, process.env.PORT)
+}
